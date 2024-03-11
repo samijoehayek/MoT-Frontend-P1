2c59b56062e1b7156c39f0af631aeb1d0917950f
Mindflares <mindflares@Mindflaress-MacBook-Pro.local>
2024-03-11 12:51:44 +0200

    Added new unity version

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -9,29 +9,30 @@ const Dashboard = () => {
 
   const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
     loaderUrl: "Test/Build/Build.loader.js",
-    dataUrl: "Test/Build/Build.data",
-    frameworkUrl: "Test/Build/Build.framework.js",
-    codeUrl: "Test/Build/Build.wasm",
+    dataUrl: "Test/Build/Build.data.unityweb",
+    frameworkUrl: "Test/Build/Build.framework.js.unityweb",
+    codeUrl: "Test/Build/Build.wasm.unityweb",
   });
 
   const router = useRouter();
 
-  // useEffect(() => {
-  //   const urlParams = new URLSearchParams(window.location.search);
-  //   const token = urlParams.get("token")
-  //     ? urlParams.get("token")
-  //     : localStorage.getItem("token");
-  //   getUserByJWT(token)
-  //     .then(() => {
-  //       localStorage.setItem("token", token);
-  //       document.cookie = `token=${token}`;
-  //       setLoadWebGL(true);
-  //     })
-  //     .catch((error) => {
-  //       console.log("Login failed: ", error);
-  //       router.push("/");
-  //     });
-  // }, []);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get("token")
+      ? urlParams.get("token")
+      : localStorage.getItem("token");
+      setLoadWebGL(true);
+    // getUserByJWT(token)
+    //   .then(() => {
+    //     localStorage.setItem("token", token);
+    //     document.cookie = `token=${token}`;
+    //     setLoadWebGL(true);
+    //   })
+    //   .catch((error) => {
+    //     console.log("Login failed: ", error);
+    //     router.push("/");
+    //   });
+  }, []);
 
   return (
     loadWebGL && (
