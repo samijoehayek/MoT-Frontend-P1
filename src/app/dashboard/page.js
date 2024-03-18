65fc80ebb65689c0d3c5dc8fd41af0bb2b754bea
Mindflares <mindflares@Mindflaress-MacBook-Pro.local>
2024-03-18 14:53:30 +0200

    Added loader before unity download

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,29 @@
 "use client";
 import React, { Fragment, useState, useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
-import { useRouter } from "next/navigation";
-import { getUserByJWT } from "@/axios";
+import bg from "../../../public/images/webgl-loader.png";
+import localFont from "@next/font/local";
+
+const albertFont = localFont({
+  src: "../../../public/FSAlbertArabic-Bold.ttf",
+  display: "swap",
+});
+
+const albertFontNormal = localFont({
+  src: "../../../public/FSAlbertArabic-Regular.ttf",
+  display: "swap",
+});
 
 const Dashboard = () => {
   const [loadWebGL, setLoadWebGL] = useState(false);
+  const [sentenceIndex, setSentenceIndex] = useState(0);
+
+  const sentences = [
+    "Explore the space and meet others",
+    "Engage in conversations through chat",
+    "Interact with Ai powered assistants.",
+    "View informational displays",
+  ];
 
   const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
     loaderUrl: "Build/Build/Build.loader.js",
@@ -15,46 +33,100 @@ const Dashboard = () => {
     streamingAssetsUrl: "Build/StreamingAssets",
   });
 
-  const router = useRouter();
+  const styling = {
+    backgroundImage: `url(${bg.src})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+  };
+
+  const sentenceStyling = {
+    animation: "fadeInOut 4s linear infinite",
+    fontSize: "1.5rem",
+  };
+
+  const animationStyles = `@keyframes fadeInOut {
+    0%, 100% {
+        opacity: 0;
+    }
+    25%, 75% {
+      opacity: 1;
+    }
+  }`;
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get("token")
-      ? urlParams.get("token")
-      : localStorage.getItem("token");
+    const token = localStorage.getItem("token");
+    if (token) {
       setLoadWebGL(true);
-    // getUserByJWT(token)
-    //   .then(() => {
-    //     localStorage.setItem("token", token);
-    //     document.cookie = `token=${token}`;
-    //     setLoadWebGL(true);
-    //   })
-    //   .catch((error) => {
-    //     console.log("Login failed: ", error);
-    //     router.push("/");
-    //   });
+    }
+
+    const intervalId = setInterval(() => {
+      setSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+    }, 4000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
     loadWebGL && (
-      <Fragment>
-        {!isLoaded && (
-          <p>Loading Application .... {Math.round(loadingProgression * 100)}</p>
-        )}
-        <Unity
-          unityProvider={unityProvider}
-          style={{
-            visibility: isLoaded ? "visible" : "hidden",
-            width: "100%",
-            height: "100%",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-          }}
-        />
-      </Fragment>
+      <>
+        <style>{animationStyles}</style>
+        <Fragment>
+          {!isLoaded && (
+            <div
+              className={"w-screen h-screen flex justify-center items-center"}
+              style={styling}
+            >
+              <div className="w-3/4 flex flex-col items-center mb-16">
+                <h1
+                  className={`${albertFont.className} font-sans text-5xl font-bold text-white mb-16 text-center`}
+                >
+                  Saudi Tourism Metaverse Loading ...
+                </h1>
+                <div className="relative w-4/5 rounded-full overflow-hidden mb-2">
+                  <div
+                    className="h-1 bg-white"
+                    style={{ width: `${loadingProgression * 100}%` }}
+                  ></div>
+                </div>
+                <div className="flex justify-end text-white mt-2 mb-12">
+                  <span className="text-sm">
+                    {Math.round(loadingProgression * 100)}%
+                  </span>
+                </div>
+                <div
+                  className={`${albertFontNormal.className} mt-4 text-white text-center`}
+                  style={sentenceStyling}
+                >
+                  {sentences.map((sentence, index) => (
+                    <div
+                      key={index}
+                      className={
+                        index === sentenceIndex ? sentenceStyling : "hidden"
+                      }
+                    >
+                      {sentence}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
+          <Unity
+            unityProvider={unityProvider}
+            style={{
+              visibility: isLoaded ? "visible" : "hidden",
+              width: "100%",
+              height: "100%",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+            }}
+          />
+        </Fragment>
+      </>
     )
   );
 };
