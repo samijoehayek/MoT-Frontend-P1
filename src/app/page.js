0e6b961dade57859ff69d5a3cf6f89e7b0dbc298
Mindflares <Samijoe Hayek>
2024-07-02 13:25:12 +0300

    Removed import statements

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,6 @@ import Restaurants from "@/components/restaurants/restaurants";
 import FooterHero from "@/components/footer-hero/footer-hero";
 import Footer from "@/components/footer/footer";
 import LobbyDisplay from "@/components/lobby-display/lobby-display";
-import RadialGradientPurple from "@/components/radial-gradient-purple/radial-gradient-purple";
-import RadialGradientGreen from "@/components/radial-gradient-green/radial-gradient-green";
 import HeroBackground from "@/components/hero-background/hero-background";
 import Attractions from "@/components/attractions/attractions";
 import BusinessTourism from "@/components/business-tourism/business-tourism";
