243cf31cb7f0be1f9f36b20717cd3940b2644b2f
Mindflares <Samijoe Hayek>
2024-08-05 10:22:47 +0300

    Started google 2FA

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,12 +5,14 @@ import BackgroundImage from "@/components/background-image/background-image";
 import PasswordUpdatedSnackbar from "@/components/password-updated-snackbar/password-updated-snackbar";
 import { AppContext } from "../appContext";
 import DuplicateEmailModal from "@/components/duplicate-email-modal/duplicate-email-modal";
+import GoogleTwoStepModal from "@/components/google-two-step-modal/google-two-step-modal";
 import EmailConfirmationModal from "@/components/email-confirmation-modal/email-confirmation-modal";
 import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
   const [emailConfirmationModal, setEmailConfirmationModal] = useState(false);
   const [duplicateEmailModal, setDuplicateEmailModal] = useState(false);
+  const [googleTwoStepModal, setGoogleTwoStepModal] = useState(true);
   const [userEmail, setUserEmail] = useState("");
 
   const router = useRouter();
@@ -19,7 +21,7 @@ const LoginPage = () => {
   const handleCloseChangePasswordModal = () => {
     setPasswordChangedBC(false);
   };
-  
+
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("token");
@@ -50,9 +52,21 @@ const LoginPage = () => {
         }}
       />
 
+      {/* Modal to start two factor google auth */}
+      <GoogleTwoStepModal
+        open={googleTwoStepModal}
+        onClose={() => {
+          setGoogleTwoStepModal(false);
+        }}
+      />
+
       {/* Login Form Section */}
       <div className="w-full lg:w-1/2 flex flex-col justify-center items-center">
-        <JoinTheMetaverse setDuplicateEmailModal={setDuplicateEmailModal} setEmailConfirmationModal={setEmailConfirmationModal} setUserEmail={setUserEmail} />
+        <JoinTheMetaverse
+          setDuplicateEmailModal={setDuplicateEmailModal}
+          setEmailConfirmationModal={setEmailConfirmationModal}
+          setUserEmail={setUserEmail}
+        />
       </div>
 
       {/* Password Snacker after a password change */}
